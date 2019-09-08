096e93067215916ecb9b3d5fa03ef927f5d21702
RyanFlachman <ryan@flachman.net>
2019-09-08 14:36:05 -0600

    Add getFriends action tests

--- Actions/Actions.test.js
+++ Actions/Actions.test.js
@@ -8,5 +8,33 @@ describe('Actions', () => {
 
       expect(result).toEqual(expected)
     })
+    it('Should set argument to the events property', () => {
+      const mockEvent = {
+        id: 1,
+        name: 'ryan'
+      }
+      const expected = mockEvent
+      const result = action.getEvents(mockEvent).events
+
+      expect(result).toEqual(expected)
+    })
+  })
+  describe('getFriends', () => {
+    it('Should have the type of GET_FRIENDS', () => {
+      const expected = 'GET_FRIENDS'
+      const result = action.getFriends().type
+
+      expect(result).toEqual(expected)
+    })
+    it('Should set the argument to the friends property', () => {
+      const mockFriends = [
+        {id: 1, name: 'ryan'},
+        {id: 2, name: 'kayla'}
+      ]
+      const expected = mockFriends
+      const result = action.getFriends(mockFriends).friends
+
+      expect(result).toEqual(expected)
+    })
   })
 })
