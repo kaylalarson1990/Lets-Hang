ad4fb717366887800e8da8160eb76fee136372ec
RyanFlachman <ryan@flachman.net>
2019-09-10 14:54:25 -0600

    Add async tests

---
+++ Thunks/FriendsThunks.test.js
@@ -0,0 +1,47 @@
+import { 
+  getUserFriendsThunk,
+  mockFriendRequestThunk,
+  acceptFriendRequestThunk,
+  removeFriendThunk,
+  getPendingFriendsThunk,
+  getRequestedFriendsThunk
+} from './FriendsThunks'
+
+describe('ALL FRIENDS THUNKS', () => {
+  describe('getUserFriendsThunk', () => {
+    let mockDispatch, mockResponse
+    beforeEach(() => {
+      mockDispatch = jest.fn()
+      mockResponse = [{id: 1, name: 'ryan'}]
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockResponse)
+        })
+      })
+    })
+    it('Should return a dispatch function', async () => {
+      await getUserFriendsThunk()(mockDispatch)
+
+      expect(mockDispatch).toHaveBeenCalled()
+    })
+    it('HAPPY PATH: Should call the getFriends action', async () => { 
+      const mockAction = {
+        type: 'GET_FRIENDS',
+        friends: mockResponse
+      }
+      await getUserFriendsThunk()(mockDispatch)
+
+      expect(mockDispatch).toHaveBeenCalledWith(mockAction)
+    })
+    it('SAD PATH: Should call the hasErrored action', async () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.catch()
+      })
+      const mockAction = {
+        type: 'HAS_ERRORED',
+        error: ''
+      }
+
+    })
+  })
+})
