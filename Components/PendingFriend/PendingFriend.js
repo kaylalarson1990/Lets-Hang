34c2a3504551c92e2cac2ac9cfd3534f64419554
RyanFlachman <ryan@flachman.net>
2019-09-10 15:08:48 -0600

    Add PendingFriend component structure

---
+++ Components/PendingFriend/PendingFriend.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FloatingAction } from 'react-native-floating-action'
+import { connect } from 'react-redux'
+
+export const PendingFriend = () => {
+  return (
+
+  )
+}
+
+const mapStateToProps = store => {
+  const friends = store.pending
+}
